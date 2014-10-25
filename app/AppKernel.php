88d7a282b10e108e486e1587980a3fc1e5ffb82b
Kamil Adryjanek <kamil.adryjanek@gmail.com>
2014-10-25 22:37:52 +0200

    FOSUserBundle installed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
