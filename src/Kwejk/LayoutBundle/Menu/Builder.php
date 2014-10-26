d16a9ec2635feb37f14945a51530e90c17bbf492
Kamil Adryjanek <kamil.adryjanek@gmail.com>
2014-10-26 15:44:46 +0100

    Menu added

---
+++ src/Kwejk/LayoutBundle/Menu/Builder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Kwejk\LayoutBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Builder extends ContainerAware
+{
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+
+        $menu->addChild('Strona główna', [
+            'route' => 'kwejk_mems_list'
+        ]);
+        $menu->addChild('Poczekalnia', [
+            'uri' => '#'
+        ]);
+        $menu->addChild('Top', [
+            'uri' => '#'
+        ]);
+        $menu->addChild('Losuj', [
+            'uri' => '#'
+        ]);
+        
+        
+        $menu->addChild('User')
+            ->setAttribute('dropdown', true);
+        $menu['User']->addChild('Profile', array('uri' => '#'))
+            ->setAttribute('divider_append', true);
+        $menu['User']->addChild('Logout', array('uri' => '#'));
+        
+        return $menu;
+    }
+}
