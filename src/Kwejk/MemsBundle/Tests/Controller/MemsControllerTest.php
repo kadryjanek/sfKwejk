6953a56e44f5651f63d607c0dd1a9751717472e0
Kamil Adryjanek <kamil.adryjanek@gmail.com>
2014-11-07 18:17:26 +0100

    Sample test case added

--- src/Kwejk/MemsBundle/Tests/Controller/MemsControllerTest.php
+++ src/Kwejk/MemsBundle/Tests/Controller/MemsControllerTest.php
@@ -9,8 +9,25 @@ class MemsControllerTest extends WebTestCase
     public function testList()
     {
         $client = static::createClient();
-
+        $client->followRedirects();
+        
         $crawler = $client->request('GET', '/');
+        
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Strona główna")')->count()
+        );
+        
+        $link = $crawler->filter('a:contains("Dodaj mema")')->eq(0)->link();
+        
+        $crawler = $client->click($link);
+        
+        // Panel logowania
+        $this->assertEquals(
+            1,
+            $crawler->filter('html:contains("Panel logowania")')->count()
+        );
+        
     }
 
     public function testShow()
